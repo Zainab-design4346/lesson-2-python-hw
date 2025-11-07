b896731577bab21d458b00d3362ead71ef0314bd
Zainab-design4346 <zainabshahzad.4346@gmail.com>
2025-11-07 21:45:54 +0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,17 @@
+print("Hi, this is Zainab.\n")
+print("Birthdays of my five friends are\n")
+
+jannatVar="29th Oct"
+print("Jannat's birthday is on -",jannatVar)
+
+senudhiVar="12th Nov"
+print("Senudhi's birthday is on -",senudhiVar)
+
+eshaVar="3Oth Mar"
+print("Esha's birthday is on -",eshaVar)
+
+eshaalVar="19th Dec"
+print("Eshaal's birthday is on -",eshaalVar)
+
+hoorVar="23th May"
+print("Hoor's birthday is on -",hoorVar)
